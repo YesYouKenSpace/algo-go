07d693249d4ea0a7c9de352abbb92de4cfc48598
yesyouken.space <kennethtxytqw@users.noreply.github.com>
2024-01-30 23:19:51 +0800

    fun(leetcode): solved two sum

---
+++ problems/two-sum.go
@@ -0,0 +1,12 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	mapNums := map[int]int{}
+	for i, num := range nums {
+		if j, ok := mapNums[target-num]; ok {
+			return []int{j, i}
+		}
+		mapNums[num] = i
+	}
+	return []int{}
+}
